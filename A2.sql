0f9f913dafc33e59800c4021b416f40f2b630a12
Leonardo Lecci <llecci@student.hult.edu>
2022-11-18 14:58:44 -0800

    Created a stored procedure to calculate balance sheet for specified and previous year and the percentage change

--- A2.sql
+++ A2.sql
@@ -91,4 +91,127 @@ BEGIN
     FROM final_statements;
 END $$
 
+-- CALL LL_PL_Statement(2017);
+
+
+-- create procedure for calculating the balance sheet
+
+
+DELIMITER $$
+DROP PROCEDURE if EXISTS LL_BS_Statement;
+CREATE PROCEDURE LL_BS_Statement(varCalendarYear INT)
+BEGIN
+    -- Declaring variables where I'll store my PL accounts
+    DECLARE i INT;
+    DECLARE statement_section VARCHAR(35);
+    DECLARE a DOUBLE;
+    DECLARE b DOUBLE;
+    -- setting year
+    SET @year = varCalendarYear;
+    SET @prev_year = varCalendarYear - 1;
+    -- setting company id to 1
+    SET @comp_id = 1;
+    -- dummy var saying if we are getting values for BS (1), or PL (0)
+    SET @BS = 1;
+    -- t is the counter to offset the returned statement section id in the WHERE subquery
+    SET @t = 0;
+    -- i si the counter fot he while loop
+    SET i = -(SELECT COUNT(*)
+              FROM statement_section
+              WHERE company_id = 1
+                AND is_balance_sheet_section = 1);
+    -- creating table to store the final PL statements with percentage changes
+    DROP TABLE if EXISTS final_BS_statements;
+    CREATE TABLE final_BS_statements
+    (
+        Account           VARCHAR(35),
+        Current_Year      VARCHAR(25),
+        Past_Year         VARCHAR(25),
+        Percentage_Change VARCHAR(25)
+    );
+    -- while loop to get all the accounts of the PL statements for the specified and its previous year
+    -- calculate the percentage change and storing all in the table final_statements
+    WHILE i < 0
+        DO
+            -- getting the field to put in the table
+            SET @sql =
+                    'SET @field = (SELECT statement_section FROM statement_section WHERE is_balance_sheet_section = ? AND company_id = ? LIMIT ?, 1)';
+            PREPARE stmt FROM @sql;
+            EXECUTE stmt USING @BS, @comp_id, @t;
+
+            IF @field LIKE ('%ASSETS%') THEN
+                SET @SQL = 'SET @amount = (SELECT IFNULL((SUM(jeli.debit) - SUM(jeli.credit)), 0)
+                           FROM account
+                                    INNER JOIN journal_entry_line_item AS jeli
+                                               ON account.account_id = jeli.account_id
+                                    INNER JOIN journal_entry AS je
+                                               ON jeli.journal_entry_id = je.journal_entry_id
+                           WHERE balance_sheet_section_id = (SELECT statement_section_id FROM statement_section WHERE is_balance_sheet_section = ? AND company_id = ? LIMIT ?, 1)
+                             AND YEAR(je.entry_date) = ?
+                             AND je.company_id = ?)';
+                PREPARE stmt FROM @sql;
+                EXECUTE stmt USING @BS, @comp_id, @t, @Year, @comp_id;
+                DEALLOCATE PREPARE stmt;
+                -- querying for year previous to specified
+                SET @SQL = 'SET @amount_prev_year = (SELECT IFNULL((SUM(jeli.debit) - SUM(jeli.credit)), 0)
+                           FROM account
+                                    INNER JOIN journal_entry_line_item AS jeli
+                                               ON account.account_id = jeli.account_id
+                                    INNER JOIN journal_entry AS je
+                                               ON jeli.journal_entry_id = je.journal_entry_id
+                           WHERE balance_sheet_section_id = (SELECT statement_section_id FROM statement_section WHERE is_balance_sheet_section = ? AND company_id = ? LIMIT ?, 1)
+                             AND YEAR(je.entry_date) = ?
+                             AND je.company_id = ?)';
+                PREPARE stmt FROM @sql;
+                EXECUTE stmt USING @BS, @comp_id, @t, @prev_year, @comp_id;
+                DEALLOCATE PREPARE stmt;
+
+            ELSE
+                SET @SQL = 'SET @amount = (SELECT IFNULL((SUM(jeli.credit) - SUM(jeli.debit)), 0)
+                           FROM account
+                                    INNER JOIN journal_entry_line_item AS jeli
+                                               ON account.account_id = jeli.account_id
+                                    INNER JOIN journal_entry AS je
+                                               ON jeli.journal_entry_id = je.journal_entry_id
+                           WHERE balance_sheet_section_id = (SELECT statement_section_id FROM statement_section WHERE is_balance_sheet_section = ? AND company_id = ? LIMIT ?, 1)
+                             AND YEAR(je.entry_date) = ?
+                             AND je.company_id = ?)';
+                PREPARE stmt FROM @sql;
+                EXECUTE stmt USING @BS, @comp_id, @t, @Year, @comp_id;
+                DEALLOCATE PREPARE stmt;
+                -- querying for year previous to specified
+                SET @SQL = 'SET @amount_prev_year = (SELECT IFNULL((SUM(jeli.credit) - SUM(jeli.debit)), 0)
+                           FROM account
+                                    INNER JOIN journal_entry_line_item AS jeli
+                                               ON account.account_id = jeli.account_id
+                                    INNER JOIN journal_entry AS je
+                                               ON jeli.journal_entry_id = je.journal_entry_id
+                           WHERE balance_sheet_section_id = (SELECT statement_section_id FROM statement_section WHERE is_balance_sheet_section = ? AND company_id = ? LIMIT ?, 1)
+                             AND YEAR(je.entry_date) = ?
+                             AND je.company_id = ?)';
+                PREPARE stmt FROM @sql;
+                EXECUTE stmt USING @BS, @comp_id, @t, @prev_year, @comp_id;
+                DEALLOCATE PREPARE stmt;
+
+            END IF;
+            -- converting global var into FLOAT var
+            SET a = CAST(@amount AS DECIMAL(65, 2));
+            SET b = CAST(@amount_prev_year AS DECIMAL(65, 2));
+            SET @perc_change = CONCAT(FORMAT(IFNULL(((a - b) / b) * 100, 0), 2), '%');
+            -- insert data into table
+            INSERT INTO final_BS_statements
+            VALUES (@field, ROUND(@amount, 2), ROUND(@amount_prev_year, 2), @perc_change);
+            SET @t = @t + 1;
+            SET i = i + 1;
+        END WHILE;
+
+
+    SELECT *
+    FROM final_BS_statements;
+END $$
+
+
+CALL LL_BS_Statement(2014);
 CALL LL_PL_Statement(2017);
+
+
